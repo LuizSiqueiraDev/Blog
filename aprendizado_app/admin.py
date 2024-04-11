fdcc7d868d2ea359ef9810a2060dadb876f81dfc
luiz siqueira <luiz.siqueira1234@gmail.com>
2024-04-11 15:38:20 -0300

    Topicos e descrições

--- aprendizado_app/admin.py
+++ aprendizado_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Topico
+from .models import Topico, Descricao
 
 
 admin.site.register(Topico)
+admin.site.register(Descricao)
--- aprendizado_app/models.py
+++ aprendizado_app/models.py
@@ -9,3 +9,19 @@ class Topico(models.Model):
         """Retorna uma representação de string do modelo."""
         return self.texto
 
+
+class Descricao(models.Model):
+    """Algo específico aprendido sobre o tópico."""
+    topico = models.ForeignKey(Topico, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_adicionada = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'descricoes'
+
+    def __str__(self):
+        """Retorna uma simples string representando a descrição."""
+        if len(self.texto) > 50:
+            return f"{self.texto[:50]}..."
+        else:
+            return self.texto
